5d0bd1f9ffa9cb0943687c1ff778939070eb3eb9
slient-coder <silent-coder@unisat.io>
2026-02-24 09:37:50 +0800

    fix(keyring-service): move types to wallet-shared

--- packages/keyring-service/src/types/keyring.ts
+++ packages/keyring-service/src/types/keyring.ts
@@ -1,4 +1,4 @@
-import { AddressType } from '@unisat/wallet-types'
+export { ADDRESS_TYPES, KeyringType } from '@unisat/wallet-shared'
 import { CosmosSignDataType } from './cosmos'
 
 // ToSignInput interface defined here to avoid circular imports
@@ -63,71 +63,3 @@ export interface Keyring {
   }): Promise<{ type: string; cbor: string; requestId: string }>
   parseSignCosmosUr?(type: string, cbor: string): Promise<any>
 }
-
-// Keyring types
-export enum KeyringType {
-  HdKeyring = 'HD Key Tree',
-  SimpleKeyring = 'Simple Key Pair',
-  KeystoneKeyring = 'Keystone',
-  ColdWalletKeyring = 'Cold Wallet',
-  ReadonlyKeyring = 'Readonly',
-  Empty = 'Empty',
-}
-
-export const ADDRESS_TYPES: {
-  value: AddressType
-  label: string
-  name: string
-  hdPath: string
-  displayIndex: number
-  isUnisatLegacy?: boolean
-}[] = [
-  {
-    value: AddressType.P2PKH,
-    label: 'P2PKH',
-    name: 'Legacy (P2PKH)',
-    hdPath: "m/44'/0'/0'/0",
-    displayIndex: 3,
-    isUnisatLegacy: false,
-  },
-  {
-    value: AddressType.P2WPKH,
-    label: 'P2WPKH',
-    name: 'Native Segwit (P2WPKH)',
-    hdPath: "m/84'/0'/0'/0",
-    displayIndex: 0,
-    isUnisatLegacy: false,
-  },
-  {
-    value: AddressType.P2TR,
-    label: 'P2TR',
-    name: 'Taproot (P2TR)',
-    hdPath: "m/86'/0'/0'/0",
-    displayIndex: 2,
-    isUnisatLegacy: false,
-  },
-  {
-    value: AddressType.P2SH_P2WPKH,
-    label: 'P2SH-P2WPKH',
-    name: 'Nested Segwit (P2SH-P2WPKH)',
-    hdPath: "m/49'/0'/0'/0",
-    displayIndex: 1,
-    isUnisatLegacy: false,
-  },
-  {
-    value: AddressType.M44_P2WPKH,
-    label: 'P2WPKH',
-    name: 'Native SegWit (P2WPKH)',
-    hdPath: "m/44'/0'/0'/0",
-    displayIndex: 4,
-    isUnisatLegacy: true,
-  },
-  {
-    value: AddressType.M44_P2TR,
-    label: 'P2TR',
-    name: 'Taproot (P2TR)',
-    hdPath: "m/44'/0'/0'/0",
-    displayIndex: 5,
-    isUnisatLegacy: true,
-  },
-]
--- packages/wallet-shared/src/constants/common.ts
+++ packages/wallet-shared/src/constants/common.ts
@@ -1,11 +1,75 @@
-import { KeyringType } from '@unisat/keyring-service/types'
+// Keyring types
+export enum KeyringType {
+  HdKeyring = 'HD Key Tree',
+  SimpleKeyring = 'Simple Key Pair',
+  KeystoneKeyring = 'Keystone',
+  ColdWalletKeyring = 'Cold Wallet',
+  ReadonlyKeyring = 'Readonly',
+  Empty = 'Empty',
+}
 
 import { AddressType, ChainType, NetworkType } from '@unisat/wallet-types'
 
 import { RestoreWalletType } from '../types'
 import { SupportedLocale } from '../types/i18n'
 
-export { ADDRESS_TYPES } from '@unisat/keyring-service/types'
+export const ADDRESS_TYPES: {
+  value: AddressType
+  label: string
+  name: string
+  hdPath: string
+  displayIndex: number
+  isUnisatLegacy?: boolean
+}[] = [
+  {
+    value: AddressType.P2PKH,
+    label: 'P2PKH',
+    name: 'Legacy (P2PKH)',
+    hdPath: "m/44'/0'/0'/0",
+    displayIndex: 3,
+    isUnisatLegacy: false,
+  },
+  {
+    value: AddressType.P2WPKH,
+    label: 'P2WPKH',
+    name: 'Native Segwit (P2WPKH)',
+    hdPath: "m/84'/0'/0'/0",
+    displayIndex: 0,
+    isUnisatLegacy: false,
+  },
+  {
+    value: AddressType.P2TR,
+    label: 'P2TR',
+    name: 'Taproot (P2TR)',
+    hdPath: "m/86'/0'/0'/0",
+    displayIndex: 2,
+    isUnisatLegacy: false,
+  },
+  {
+    value: AddressType.P2SH_P2WPKH,
+    label: 'P2SH-P2WPKH',
+    name: 'Nested Segwit (P2SH-P2WPKH)',
+    hdPath: "m/49'/0'/0'/0",
+    displayIndex: 1,
+    isUnisatLegacy: false,
+  },
+  {
+    value: AddressType.M44_P2WPKH,
+    label: 'P2WPKH',
+    name: 'Native SegWit (P2WPKH)',
+    hdPath: "m/44'/0'/0'/0",
+    displayIndex: 4,
+    isUnisatLegacy: true,
+  },
+  {
+    value: AddressType.M44_P2TR,
+    label: 'P2TR',
+    name: 'Taproot (P2TR)',
+    hdPath: "m/44'/0'/0'/0",
+    displayIndex: 5,
+    isUnisatLegacy: true,
+  },
+]
 
 export const KEYRING_CLASS = {
   PRIVATE_KEY: 'Simple Key Pair',
